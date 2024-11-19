bd12dc564cb2f4724396951b2a9ecd96735b6e3b
therealkylepacheco <kpacheco@nnu.edu>
2024-11-18 18:07:24 -0700

    Using decimal

--- src/components/InputNumberTheme.tsx
+++ src/components/InputNumberTheme.tsx
@@ -7,6 +7,6 @@ export const InputNumberStyled = styled(InputNumber<number>)`
 
 export const InputNumberTheme = (props: InputNumberProps<number>) => (
   <Tooltip placement="topLeft" title={props.placeholder} trigger={["focus"]}>
-    <InputNumberStyled inputMode="numeric" size="large" {...props} />
+    <InputNumberStyled inputMode="decimal" size="large" {...props} />
   </Tooltip>
 );
